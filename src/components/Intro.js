e7278ca9e0e908a04c0f5932019870bf4048633e
Jueunkim429 <jueunkim429@gmail.com>
2022-11-11 03:21:09 +0900

    [feat] intro 추가

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,7 @@ import default_Img11 from "./main.png";
 import default_Img2 from "./step1.png";
 import default_Img3 from "./step2.png";
 import default_Img4 from "./step3.png";
+import default_Img5 from "./step4.png";
 function Intro() {
     const onErrorImg11 = (e) => {
         e.target.src = default_Img11;
@@ -17,6 +18,9 @@ function Intro() {
     const onErrorImg4 = (e) => {
         e.target.src = default_Img4;
     }
+    const onErrorImg5 = (e) => {
+        e.target.src = default_Img5;
+    }
     return(
         <>
         <img className="intro_logo" src="img/main.png" onError={onErrorImg11}/>
@@ -24,7 +28,7 @@ function Intro() {
         <img className="intro_step" src="img/step1.png" onError={onErrorImg2}/>
         <img className="intro_step" src="img/step2.png" onError={onErrorImg3}/>
         <img className="intro_step" src="img/step3.png" onError={onErrorImg4}/>
-
+        <img className="intro_step" src="img/step4.png" onError={onErrorImg5}/>
         </>
     );
 }
